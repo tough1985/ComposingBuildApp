b04308957a92324fd2f4b6c8ded59ff71bccd131
刘坤 <liukun@fangdd.com>
2020-09-16 18:24:07 +0800

    [NFF] 添加includeBuild设置

--- app/src/main/java/me/xiba/composingbuildapp/MainActivity.java
+++ app/src/main/java/me/xiba/composingbuildapp/MainActivity.java
@@ -3,6 +3,9 @@ package me.xiba.composingbuildapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import me.xiba.composinglibrary.Constanst;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((TextView)findViewById(R.id.tv_content)).setText(Constanst.TAG);
     }
 }
